0185cccd04f421192884d8d947fe6c5e6b248c2c
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-10 13:46:46 +0700

    Add a doc string to function lcm()

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,11 +1,16 @@
 def lcm(a, b):
+    """
+    Input: 2 integers a, b
+    Output: the least common multiple of a and b
+
+    Property: lcm(a, b) * gcd(a, b) = a * b
+
+    # naive solution
     # for l in range(1, a * b + 1):
     #     if l % a == 0 and l % b == 0: # (if l is divisible by a and l is divisible by b)
     #         return l
-
     # assert False
-
-    # Property: lcm(a, b) * gcd(a, b) = a * b
+    """
     # Step 1: Compute gcd(a, b)
     def gcd(a, b):
         if a == 0:
