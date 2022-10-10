887720586ab000c36c697696b050cc981a393d99
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-10 14:18:54 +0700

    Add a doc string to function fibonacci_sum(), time complexity

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,4 +1,14 @@
 def fibonacci_sum(n):
+    """
+    Input: An integer n
+    Output: The last digit of  S_n = F0 + F1 + ... + Fn
+
+    Property 1: S_n = F_(n+2) - 1
+    Property 2: The last digits of the Fibonacci sequence repeat in a cycle of 60
+    
+    Formula S_n: https://www.youtube.com/watch?v=VKc7qUdurMk&ab_channel=DrPeyam
+
+    # naive solution
     # if n <= 1:
     #     return n
 
@@ -9,9 +19,8 @@ def fibonacci_sum(n):
     #     _sum += current
 
     # return _sum % 10
+    """
     
-    # Property 1: S_n = F_(n+2) - 1
-    # Property 2: The last digits of the Fibonacci sequence repeat in a cycle of 60
     last_digits = [0] * 60
     last_digits[0] = 0
     last_digits[1] = 1
@@ -19,7 +28,7 @@ def fibonacci_sum(n):
         last_digits[i] = (last_digits[i - 1] + last_digits[i - 2]) % 10
     
     if last_digits[(n + 2) % 60] == 0:
-        return 9
+        return 9 # because of S_n = F_(n+2) - 1
     else:
         return last_digits[(n + 2) % 60] - 1
 
@@ -27,3 +36,5 @@ def fibonacci_sum(n):
 if __name__ == '__main__':
     n = int(input())
     print(fibonacci_sum(n))
+
+# Time complexity: O(1)
