b43ebd5cd0ebcf944d168d373a7ee47b66c7d61e
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-10 12:15:39 +0700

    Add a doc string to function max_pairwise_product, time complexity

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,14 +1,22 @@
 def max_pairwise_product(numbers):
-    n = len(numbers)
+    """
+    Input: A list of non-negative integers
+    Output: The product of two maximum values in the list (integer)
+    # Naive method O(n^2)
     # for first in range(n):
     #     for second in range(first + 1, n):
     #         max_product = max(max_product,
     #             numbers[first] * numbers[second])
+    """
+    # Time: O(n)
+    n = len(numbers)
+    # Find the first max
     first = -1
     for i in range(n):
         if first == -1 or numbers[i] >= numbers[first]:
             first = i
     
+    # Find the second max, excluded the first max
     second = -1
     for j in range(n):
         if (j != first) and (second == -1 or numbers[j] >= numbers[second]):
