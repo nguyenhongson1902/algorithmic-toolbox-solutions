85526794ceefb4ccb46ba94823f63e101dbdd216
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-10 13:43:24 +0700

    Add a doc string to function gcd()

--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,10 +1,17 @@
 def gcd(a, b):
+    """
+    Input: 2 integers a, b
+    Output: the greatest common divisor between a and b
+
+    # naive solution
     # current_gcd = 1
     # for d in range(2, min(a, b) + 1):
     #     if a % d == 0 and b % d == 0:
     #         if d > current_gcd:
     #             current_gcd = d
     # return current_gcd
+
+    """
 
     if a == 0:
         return b
@@ -15,10 +22,6 @@ def gcd(a, b):
         return gcd(a % b, b)
     else:
         return gcd(a, b % a)
-
-
-    
-
 
 if __name__ == "__main__":
     a, b = map(int, input().split())
