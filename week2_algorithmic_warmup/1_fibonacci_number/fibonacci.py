ef3d50bcfc4085ac5b6bcfb62c3ff9fcd032ab30
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-10 12:47:20 +0700

    Add a doc string to function fibonacci_number

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,4 +1,12 @@
 def fibonacci_number(n):
+    """
+    This is the iterative solution to the problem, O(n) time
+    Input: An integer n
+    Output: The n-th Fibonacci number
+
+    Visualization: https://www.cs.usfca.edu/~galles/visualization/DPFib.html
+    Recursive solution: O(2^n) time
+    """
     if n == 0:
         return 0
 
@@ -7,7 +15,7 @@ def fibonacci_number(n):
     fib_numbers[1] = 1
 
     for i in range(2, len(fib_numbers)): # Loop run in O(n) times
-        fib_numbers[i] = fib_numbers[i-1] + fib_numbers[i-2] # O(n) each time
+        fib_numbers[i] = fib_numbers[i-1] + fib_numbers[i-2] # O(1)
     
     return fib_numbers[-1]
 
@@ -17,4 +25,4 @@ if __name__ == '__main__':
     input_n = int(input())
     print(fibonacci_number(input_n))
 
-# Time complexity: O(n^2)
+# Time complexity: O(n)
