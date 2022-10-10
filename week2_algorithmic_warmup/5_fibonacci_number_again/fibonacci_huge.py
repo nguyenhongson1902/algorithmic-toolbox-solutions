e27c249256de82a8df3520b0cb9cb86933eeb759
Nguyen Hong Son <nguyenhongsonk62hust@gmail.com>
2022-10-10 14:10:21 +0700

    Add explaination to a doc string in fibonacci_huge() function

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,4 +1,16 @@
-def fibonacci_huge_naive(n, m):
+def fibonacci_huge(n, m):
+    """
+    Input: 2 integers n, m
+    Output: The n-th Fibonacci number modulo m
+
+    To compute, say, F2015 mod 3 we just need to find the remainder of 2015
+    when divided by 8. Since 2015 = 251*8 + 7, we conclude that 
+    F2015 mod 3 = F7 mod 3 = 1
+
+    Read Learning Algorithms Through Programming and Puzzle Solving slides
+    Pisano period: https://www.geeksforgeeks.org/fibonacci-number-modulo-m-and-pisano-period/
+
+    # naive solution
     # if n <= 1:
     #     return n
 
@@ -9,7 +21,10 @@ def fibonacci_huge_naive(n, m):
     #     previous, current = current, previous + current
 
     # return current % m
-
+    """
+    # The period always starts with 01
+    # Fi mod m is periodic for every m
+    # the period of Fi mod m does not exceed m^2
     previous, current = 0, 1
     for i in range(m*m):
         previous, current = current, (previous + current) % m
@@ -28,8 +43,6 @@ def fibonacci_huge_naive(n, m):
     
     return current_mod
 
-
-
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    print(fibonacci_huge_naive(n, m))
+    print(fibonacci_huge(n, m))
